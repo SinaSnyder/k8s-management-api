e8508027b7044a52d1240e11cb15517d9467e576
SinaSnyder <sinaaa733@gmail.com>
2026-08-22 15:25:08 +0330

    tasks.py updated
    implementing celery task
    1.Status update to Running
    2.Finding the pods related to an app in k8s
    3.Create local directory /backups/{app_id}/{yyyy-mm-dd}/
    4.Running the backup command from the pod to the local worker
    5.Change status to completed or failed

--- backup/tasks.py
+++ backup/tasks.py
@@ -1,9 +1,68 @@
+import os
+import tarfile
+from datetime import datetime
 from celery import shared_task
-import time
+from django.utils import timezone
+from .models import Backup
+from clusters.k8s_client import get_k8s_client
+from kubernetes.stream import stream
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def execute_backup_task(self, backup_db_id):
+    try:
+        backup = Backup.objects.get(id=backup_db_id)
+        backup.status = 'running'
+        backup.save()
+
+        app = backup.app
+        namespace = app.namespace.name
+        cluster = app.namespace.cluster
+
+        k8s_api = get_k8s_client(cluster)
+        pods = k8s_api.list_namespaced_pod(namespace=namespace, label_selector=f"app={app.name}")
+        
+        if not pods.items:
+            raise Exception("no enable pod for this app")
+
+        pod_name = pods.items[0].metadata.name
+
+        today_str = datetime.now().strftime('%Y-%m-%d')
+        backup_dir = f"/backups/{app.id}/{today_str}"
+        os.makedirs(backup_dir, exist_ok=True)
+        file_path = os.path.join(backup_dir, f"{backup.backup_id}.tar.gz")
+
+        exec_command = ['tar', 'cfz', '-', backup.source_path]
+        resp = stream(
+            k8s_api.connect_get_namespaced_pod_exec,
+            pod_name,
+            namespace,
+            command=exec_command,
+            stderr=True, stdin=False,
+            stdout=True, tty=False,
+            _preload_content=False
+        )
+
+        with open(file_path, 'wb') as f:
+            while resp.is_open():
+                resp.update(timeout=1)
+                if resp.peek_stdout():
+                    f.write(resp.read_stdout().encode('utf-8'))
+
+        backup.status = 'completed'
+        backup.save()
+        return f"Backup {backup.backup_id} completed successfully."
+
+    except Exception as exc:
+        backup = Backup.objects.get(id=backup_db_id)
+        backup.status = 'failed'
+        backup.save()
+        raise self.retry(exc=exc)
+
 
 @shared_task
-def run_backup_task():
-    print("---backup in progress---")
-    time.sleep(5)  
-    print("---backup completed successfully---")
-    return "Backup completed successfully"
+def cleanup_stale_backups():
+    threshold = timezone.now() - timezone.timedelta(hours=24)
+    stale_backups = Backup.objects.filter(status='pending', created_at__lt=threshold)
+    stale_backups.update(status='failed')
+
+
